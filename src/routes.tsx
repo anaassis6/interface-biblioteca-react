94447c1428de9fcab3e5804485362d4dc8a92bfc
anaassis6 <analuassis6@gmail.com>
2025-04-04 08:58:21 -0300

    criando rotas

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PHome from './pages/PHome/PHome';
+import PLogin from './pages/PLogin/PLogin';
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/APP_ROUTES.ROUTE_HOME' element={<PHome />}></Route>
+                <Route path='/APP_ROUTES.ROUTE_LOGIN' element={<PLogin />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
